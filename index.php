417239242afcbbd983fddcbea2237a8d0b933ca3
ericzon <ericzon@gmail.com>
2013-02-06 12:49:37 +0100

    Texto actualizado post merge 1

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-    		Estoy en la alternativa y he añadido un par de imágenes más.
+    		Vuelvo a estar en la principal, después del primer merge.
             <img src="imgs/tentacle.jpg" />
             <img src="imgs/ondho_300.jpg" />
             <img src="imgs/ondho_logo.jpg" />
