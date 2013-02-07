d7543ff4e073c2034433a05c05089060a72501c4
ericzon <ericzon@gmail.com>
2013-02-07 08:33:10 +0100

    texto nuevo

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-    		Vuelvo a estar en la principal, después del primer merge.
+    		Vuelvo a estar en la principal. Ahora voy a editar el fichero que está en el repo, provocando un merge.
             <img src="imgs/tentacle.jpg" />
             <img src="imgs/ondho_300.jpg" />
             <img src="imgs/ondho_logo.jpg" />
