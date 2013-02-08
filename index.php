8155893f81abaac7636c21cfac08f68c86ef2424
Eric xyz <ericzon@gmail.com>
2013-02-08 08:10:34 +0100

    img borrada, texto cambiado

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-    		Arreglado el conflicto de Sergi, por fin?
+    		Sin conflictos, por fin. Más y más cambios
             <img src="imgs/tentacle.jpg" />
             <img src="imgs/ondho_300.jpg" />
             <img src="imgs/ondho_logo.jpg" />
