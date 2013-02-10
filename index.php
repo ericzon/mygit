81dfb071dced0bbb34959a8cd5fce493557cb411
ericzon <ericzon@gmail.com>
2013-02-10 23:51:43 +0100

    post imgsmergeadas

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-veo los conflictos en el remoto y los arreglo, subiendo todos los merges locales
+commit post imgs mergeadas
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
