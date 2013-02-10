912287c8aefe2cf31c5978dfa00b65b498fa83b3
Eric xyz <ericzon@gmail.com>
2013-02-10 19:49:11 +0100

    mas cambios

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-line parched
++ cambios
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
