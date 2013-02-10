18f3a6f60ddb5efdade0d777d78dbb2f33910dbf
Eric xyz <ericzon@gmail.com>
2013-02-10 13:07:01 +0100

    otro archivo

---
+++ otro1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8" />
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+      <title>MyGit Test</title>
+      <?php
+      
+      ?>
+    </head>
+    <body>
+  <header>  
+    <h1>Git Test</h1>
+    <!-- Header content -->
+    
+  </header>
+  <div role="main">
+    <!-- Page content -->
+    <section>
+    	<article>
+Este es otro archivo aparte
+    	</article>	
+    </section>
+  </div>
+  <aside role="complementary">
+    <!-- Secondary content -->
+ 
+  </aside>
+  <footer>
+    <!-- Footer content -->
+  </footer>
+</body>
+</html>
