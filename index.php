cafe3638b9c33aecb7782411925b3427d9b30107
Eric xyz <ericzon@gmail.com>
2013-02-10 20:06:23 +0100

    commit en raming

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-+ cambios
+commit en raming
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
