f95adf406414bbed02893bbd9f9e123aafa0c9a7
Eric xyz <ericzon@gmail.com>
2013-02-10 20:04:26 +0100

    otra bifurcacion creada

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-+ cambios
+2a bifurcacion
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
