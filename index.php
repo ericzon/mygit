cf3b12cb95e7a33e8ab8d5fbfa327658e16cbc8e
Eric xyz <ericzon@gmail.com>
2013-02-10 20:15:51 +0100

    commit post merge

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-merge raming y raming2 y me quedo en raming
+merge raming - raming2, me quedo en raming y hago otro commit
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
