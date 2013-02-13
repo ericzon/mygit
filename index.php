199727eed1e8de2a52b729a9739d1d7bc01fd356
Eric xyz <ericzon@gmail.com>
2013-02-14 00:56:01 +0100

    otro mas

--- index.php
+++ index.php
@@ -19,7 +19,11 @@
     <section>
     	<article>
 <<<<<<< HEAD
+<<<<<<< HEAD
 arreglado
+=======
+post imgs merge
+>>>>>>> parent of a1ff16f... otro commit
 =======
 post imgs merge
 >>>>>>> parent of a1ff16f... otro commit
