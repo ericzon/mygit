a1ff16f29d13d1d7c56d191e38a8356643d693ae
Eric xyz <ericzon@gmail.com>
2013-02-14 00:26:13 +0100

    otro commit

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-post imgs merge
+post imgs2 merge
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
