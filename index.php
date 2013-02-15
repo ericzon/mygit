e9f43f562b7638a7e19da4f26d1076f70cfca88b
Eric xyz <ericzon@gmail.com>
2013-02-15 08:02:15 +0100

    subida

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Page content -->
     <section>
     	<article>
-arreglado
+post imgs merge
             <img src="imgs/tentacle.jpg" />
 			<img src="imgs/marcianito.png" />
 			<img src="imgs/robot.gif" />
